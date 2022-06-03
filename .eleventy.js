91351de0890e42174505eae3162a199d38adf749
Irene <i@ire.ne>
2022-06-03 12:21:51 -0700

    add markdown support

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,35 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./assets")
 
+  // Customize Markdown library and settings:
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      placement: "after",
+      class: "direct-link",
+      symbol: "#",
+      level: [1, 2, 3, 4],
+    }),
+    slugify: eleventyConfig.getFilter("slug")
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
   return {
     templateFormats: [
+      "md",
       "njk",
       "html",
       "liquid"
     ],
     // Pre-process *.md files with: (default: `liquid`)
     markdownTemplateEngine: "njk",
-
     // Pre-process *.html files with: (default: `liquid`)
     htmlTemplateEngine: "njk",
     passthroughFileCopy: true,
